e2507f62665e925d6e6fb3368cf3e2bd42e4fc89
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-09 09:55:01 -0400

    Time: 120 ms (80.00%), Space: 50.9 MB (100.00%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.js
+++ 1696-jump-game-vi/1696-jump-game-vi.js
@@ -4,40 +4,7 @@
  * @return {number}
  */
 var maxResult = function(nums, k) {
-  /* Brute Force *
-  const currIndex = arguments[2] || 0;
-  
-  if (currIndex >= nums.length - 1) {
-    return nums[nums.length - 1];
-  }
-  
-  let score = -Infinity;
-  
-  for (let jump = 1; jump <= k; jump++) {
-    // this can be improved by memoizing maxResult
-    score = Math.max(score, nums[currIndex] + maxResult(nums, k, currIndex + jump));
-  }
-  return score;
-  //*/
-  
-  /* Bottom-Up DP: Tabulation *
-  const results = Array(nums.length).fill(-Infinity);
-  
-  results[0] = nums[0];
-  
-  nums.forEach((num, i) => {
-    for (let j = 1; j <= k && i - j >= 0; j++) {
-      results[i] = Math.max(results[i], results[i - j] + num);
-    }
-  });
-  
-  return results.pop()
-  //*/
-  
-  /* Using a deque - Sad JavaScript has slow shift() :( */
-  const oldLast = Array.prototype.last;
-  Array.prototype.last = function() { return this[this.length - 1]; }
-  
+  /* Using a deque - Sad JavaScript has slow shift() :( */  
   const deq = [nums.length - 1]
   
   for (let i = nums.length - 2; i >= 0; i--) {
@@ -46,7 +13,7 @@ var maxResult = function(nums, k) {
     }
     nums[i] += nums[deq[0]];
     
-    while (deq.length && nums[deq.last()] <= nums[i]) {
+    while (deq.length && nums[deq[deq.length - 1]] <= nums[i]) {
       deq.pop();
     }
     
@@ -55,17 +22,3 @@ var maxResult = function(nums, k) {
   
   return nums[0]
 };
-
-const memoize = function (func) {
-  const cache = {};
-  
-  return function (...args) {
-    const key = JSON.stringify(args);
-    
-    if (!cache.hasOwnProperty(key)) {
-      cache[key] = func(...args);
-    }
-    
-    return cache[key];
-  }
-}
