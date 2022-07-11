20e5492a80797a1cabf07c3b8a0efa965cf84ebb
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-11 15:16:26 -0400

    Time: 5 ms (23.66%), Space: 2.3 MB (76.62%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.go
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.go
@@ -15,28 +15,46 @@ type depthNode struct {
 func rightSideView(root *TreeNode) []int {
     var visibleNodes []int
     
-    // === BFS ===
-    queue := []depthNode{}
+//     // === BFS ===
+//     queue := []depthNode{}
     
-    if root != nil {
-        queue = append(queue, depthNode{node: root, depth: 0})
-    }
+//     if root != nil {
+//         queue = append(queue, depthNode{node: root, depth: 0})
+//     }
     
-    for len(queue) > 0 {
-        node, depth := queue[0].node, queue[0].depth
-        queue = queue[1:] // dequeue
+//     for len(queue) > 0 {
+//         node, depth := queue[0].node, queue[0].depth
+//         queue = queue[1:] // dequeue
+        
+//         if depth == len(visibleNodes) {
+//             visibleNodes = append(visibleNodes, node.Val)
+//         }
+        
+//         if node.Right != nil {
+//             queue = append(queue, depthNode{node.Right, depth + 1})
+//         }
+//         if node.Left != nil {
+//             queue = append(queue, depthNode{node.Left, depth + 1})
+//         }
+//     }
+    
+    // === DFS ===
+    var dfs func(*TreeNode, int) 
+    
+    dfs = func(node *TreeNode, depth int) {
+        if node == nil {
+            return
+        }
         
         if depth == len(visibleNodes) {
             visibleNodes = append(visibleNodes, node.Val)
         }
         
-        if node.Right != nil {
-            queue = append(queue, depthNode{node.Right, depth + 1})
-        }
-        if node.Left != nil {
-            queue = append(queue, depthNode{node.Left, depth + 1})
-        }
+        dfs(node.Right, depth + 1)
+        dfs(node.Left, depth + 1)
     }
+    
+    dfs(root, 0)
     
     return visibleNodes
 }
