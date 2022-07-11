e88dc3695d9365b7a1b71cb827397647a8c91aa5
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-11 11:56:16 -0400

    Time: 122 ms (11.96%), Space: 43.8 MB (48.89%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  if (!root) {
+    return [];
+  }
+  
+  const visibleNodes = [];
+  const queue = [[root, 0]];
+  
+  while (queue.length) {
+    const [node, depth] = queue.shift();
+    
+    visibleNodes[depth] = node.val;
+    
+    node.left && queue.push([node.left, depth + 1]);
+    node.right && queue.push([node.right, depth + 1]);
+  }
+  
+  return visibleNodes;
+};
