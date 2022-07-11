0fbc1f33a5a713e3d332ba50a1ccd3cea98de088
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-11 11:03:27 -0400

    Time: 59 ms (98.89%), Space: 43 MB (66.83%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,19 +3,31 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-  const costToStep = _.memoize(
-    /**
-     * @param {number} index
-     * @returns {number}
-     */
-    function findCost(index) {
-      if (index < 2) {
-        return cost[index];
-      }
-
-      return cost[index] + Math.min(costToStep(index - 2), costToStep(index - 1));
-    }
-  );
+//   === Memoization ===
+//   const costToStep = _.memoize(
+//     /**
+//      * @param {number} i
+//      * @returns {number}
+//      */
+//     function (i) {
+//       if (i < 2) {
+//         return cost[i];
+//       }
+//       return cost[i] + Math.min(costToStep(i - 2), costToStep(i - 1));
+//     }
+//   );
+//   return Math.min(costToStep(cost.length - 2), costToStep(cost.length - 1));
   
-  return Math.min(costToStep(cost.length - 2), costToStep(cost.length - 1));
+//  === Tabulation ===
+  const n = cost.length
+  let costToStep = Array(n).fill(0);
+  
+  costToStep[0] = cost[0];
+  costToStep[1] = cost[1];
+  
+  for (let i = 2; i < n; i++) {
+    costToStep[i] = cost[i] + Math.min(costToStep[i - 1], costToStep[i - 2]);
+  }
+  
+  return Math.min(costToStep[n - 2], costToStep[n - 1]);
 };
