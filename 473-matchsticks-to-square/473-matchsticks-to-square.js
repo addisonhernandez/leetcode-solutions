c408236a697b8aee7f6fbde225c5957ae99b9de1
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-12 19:02:14 -0400

    Time: 1167 ms (15.94%), Space: 47.9 MB (26.09%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+var makesquare = function(matchsticks) {
+  const totalStickLength = matchsticks.reduce((sum, curr) => sum + curr, 0);
+  
+  if (totalStickLength % 4 !== 0) {
+    return false;
+  }
+  
+  matchsticks.sort((a, b) => b - a); // sort descending in-place
+  
+  const target = totalStickLength / 4;
+  
+  return (function assignSticks(index, top, bot, left, right) {
+    const sides = [top, bot, left, right];
+    if (sides.every((side) => side === target)) {
+      return true;
+    }
+    if (sides.some((side) => side > target)) {
+      return false;
+    }
+    
+    const nextStick = matchsticks[index];
+    
+    return (
+      assignSticks(index + 1, top + nextStick, bot, left, right) ||
+      assignSticks(index + 1, top, bot + nextStick, left, right) ||
+      assignSticks(index + 1, top, bot, left + nextStick, right) ||
+      assignSticks(index + 1, top, bot, left, right + nextStick)
+    );
+  })(0, 0, 0, 0, 0);
+};
