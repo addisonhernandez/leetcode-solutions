dcc941a4590ecd6b206bdeba8fec0ebd49a77235
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-18 10:03:49 -0400

    Time: 172 ms (100.00%), Space: 45.2 MB (100.00%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.ts
@@ -0,0 +1,18 @@
+function kInversePairs(n: number, k: number): number {
+  const modulus = 1e9 + 7;
+  const cache = Array.from(Array(2), () => Array(k + 1).fill(0));
+  
+  cache[0][0] = 1;
+  cache[1][0] = 1;
+  
+  for (let elems = 1; elems <= n; elems++)
+    for (let pairs = 1; pairs <= k; pairs++) {
+      cache[elems % 2][pairs] = cache[(elems - 1) % 2][pairs] + cache[elems % 2][pairs - 1];
+      if (pairs >= elems) {
+        cache[elems % 2][pairs] -= cache[(elems - 1) % 2][pairs - elems];
+      }
+      cache[elems % 2][pairs] = (cache[elems % 2][pairs] + modulus) % modulus;
+    }
+  
+  return cache[n % 2][k];
+};
