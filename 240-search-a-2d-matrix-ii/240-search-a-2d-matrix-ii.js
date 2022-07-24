2579b374842da83a266a94a1a410d3d5d8d5ac14
Addison Hernandez <addison.hernandez@gmail.com>
2022-07-23 22:35:43 -0400

    Time: 644 ms (65.76%), Space: 45.2 MB (55.08%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let r = 0;
+  let c = matrix[0].length - 1;
+  
+  while (r < matrix.length && c >= 0) {
+    if (target > matrix[r][c]) {
+      r += 1;
+    } else if (target < matrix[r][c]) {
+      c -= 1;
+    } else {
+      return true;
+    }
+  }
+  
+  return false;
+};
