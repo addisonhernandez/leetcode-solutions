8ec5684c1897ca8809d9aca27e75cdaa7a80e9c6
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-06 08:45:07 -0400

    Time: 88 ms (55.56%), Space: 42 MB (44.44%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function(buckets, minutesToDie, minutesToTest) {
+  const rounds = Math.floor(minutesToTest / minutesToDie)
+  return Math.ceil(Math.log(buckets) / Math.log1p(rounds))
+};
