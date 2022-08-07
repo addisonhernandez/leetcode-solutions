570d8989c2592055c0231515d1a09a4dda329bdd
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-07 16:49:58 -0400

    Time: 94 ms (100.00%), Space: 49.3 MB (100.00%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.ts
@@ -0,0 +1,19 @@
+function countVowelPermutation(n: number): number {
+  const modulus = 1e9 + 7;
+  const [a, e, i, o, u] = [0, 1, 2, 3, 4];
+  
+  const curr = [1, 1, 1, 1, 1];
+  
+  while (n-- > 1) {
+    const prev = [...curr];
+    curr[a] = (prev[e] + prev[i] + prev[u]) % modulus;
+    curr[e] = (prev[a] + prev[i]) % modulus;
+    curr[i] = (prev[e] + prev[o]) % modulus;
+    curr[o] = prev[i];
+    curr[u] = (prev[i] + prev[o]) % modulus;
+    // console.table({prev, curr})
+  }
+  
+  return curr.reduce((sum, val) => (sum + val) % modulus);
+
+};
