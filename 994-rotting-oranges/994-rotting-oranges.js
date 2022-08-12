7301d9f5032d6b554f92062d54e7280acdd65d5d
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-12 10:54:48 -0400

    Time: 125 ms (41.61%), Space: 44.9 MB (53.03%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  
+  const offsets = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+  
+  const isValid = (r, c) => r >= 0 && c >= 0 && r < m && c < n;
+  
+  let q = [];
+  let fresh = 0;
+  
+  grid.forEach((row, r) => {
+    row.forEach((orange, c) => {
+      if (orange === 2) {
+        q.push([r, c]);
+      } else if (orange === 1) {
+        fresh += 1;
+      }
+    });
+  });
+  
+  let minutes = 0;
+  
+  while (q.length > 0 && fresh > 0) {
+    const nextQ = [];
+    
+    q.forEach(([r, c]) => {
+      offsets.forEach(([dr, dc]) => {
+        if (isValid(r + dr, c + dc) && grid[r + dr][c + dc] === 1) {
+          grid[r + dr][c + dc] = 2;
+          fresh -= 1;
+          nextQ.push([r + dr, c + dc]);
+        }
+      });
+    });
+    
+    q = nextQ;
+    minutes += 1;
+  }
+  
+  return fresh > 0 ? -1 : minutes;
+};
