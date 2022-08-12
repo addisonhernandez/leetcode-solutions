285490fb26bb11651726d07685787fe19dbee9cf
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-11 21:47:59 -0400

    Time: 21 ms (79.49%), Space: 7.5 MB (43.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.go
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.go
@@ -8,11 +8,13 @@
  */
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    if p.Val < root.Val && q.Val < root.Val {
-        return lowestCommonAncestor(root.Left, p, q)
+    for {
+        if p.Val < root.Val && q.Val < root.Val {
+            root = root.Left
+        } else if p.Val > root.Val && q.Val > root.Val {
+            root = root.Right
+        } else {
+            return root
+        }
     }
-    if p.Val > root.Val && q.Val > root.Val {
-        return lowestCommonAncestor(root.Right, p, q)
-    }
-    return root
 }
