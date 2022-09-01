7c3ddd4ad968ecf9e7ad0f1138c0079cddddb8ec
Addison Hernandez <addison.hernandez@gmail.com>
2022-08-31 22:04:55 -0400

    Time: 175 ms (11.38%), Space: 10.4 MB (88.02%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func goodNodes(root *TreeNode) int {
+    return dfs(root, root.Val)
+}
+
+func dfs(root *TreeNode, pathMax int) int {
+    if (root == nil) {
+        return 0
+    }
+    
+    var good int
+    
+    if root.Val >= pathMax {
+        good = 1
+        pathMax = root.Val
+    }
+    
+    return good + dfs(root.Left, pathMax) + dfs(root.Right, pathMax)
+}
