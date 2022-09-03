b80c3ab756498d3485ec522fcee3ba094040b910
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-03 08:52:58 -0400

    Time: 77 ms (100.00%), Space: 45 MB (50.00%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.ts
@@ -0,0 +1,21 @@
+function numsSameConsecDiff(n: number, k: number): number[] {
+  let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  for (let i = 1; i < n; i++) {
+    const nextNums = []
+
+    nums.forEach((num) => {
+      const leastDigit = num % 10
+      if (leastDigit + k < 10) {
+        nextNums.push(10 * num + leastDigit + k)
+      }
+      if (leastDigit >= k && k !== 0) {
+        nextNums.push(10 * num + leastDigit - k)
+      }
+    })
+
+    nums = nextNums
+  }
+
+  return nums
+}
