24576284ea29cc95be8a9a2bbbdd7b062b703066
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-05 22:42:35 -0400

    Time: 35 ms (88.42%), Space: 13.9 MB (14.68%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,10 +1,20 @@
+from heapq import heapify, heappush, heappop
+
+
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones.sort()
-
-        while len(stones) > 1:
-            *stones, x, y = stones
-            if x != y:
-                bisect.insort(stones, y - x)
-            
-        return stones[0] if stones else 0
+        '''O(n^2) using bisect.insort'''
+        # stones.sort()
+        # while len(stones) > 1:
+        #     *stones, x, y = stones
+        #     bisect.insort(stones, y - x)
+        # return stones[0]
+        
+        '''O(nlog(n)) using a hacky maxheap'''
+        heap = [-s for s in stones]  # minheap of negatives is a maxheap
+        heapify(heap)
+        
+        while len(heap) > 1:
+            heappush(heap, heappop(heap) - heappop(heap))
+        
+        return -heap[0]
