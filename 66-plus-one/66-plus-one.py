084a3cee00f0c64434faaf5d4260013fe41b4b17
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-06 00:00:43 -0400

    Time: 73 ms (5.37%), Space: 13.8 MB (96.28%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,16 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
-        
         for i in range(len(digits))[::-1]:
-            if not carry:
-                break
-            carry, digits[i] = carry_add(carry, digits[i])
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
         
-        return [1] + digits if carry else digits
+        return [1] + digits
     
-    
-def carry_add(a: int, b: int) -> Tuple[int, int]:
-    sum = a + b
-    carry = 1 if sum > 9 else 0
-    return (carry, sum % 10)
