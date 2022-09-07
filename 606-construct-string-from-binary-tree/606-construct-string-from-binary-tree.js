6d4d1a1c1daeae978b42aec70d3e6f5c0c25b450
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-07 15:39:08 -0400

    Time: 145 ms (7.65%), Space: 47.6 MB (25.14%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function(root) {
+    if (!root) {
+        return ''
+    }
+    
+    repr = '' + root.val
+    
+    if (root.left || root.right) {
+        repr += `(${tree2str(root.left)})`
+    }
+    if (root.right) {
+        repr += `(${tree2str(root.right)})`
+    }
+    
+    return repr
+};
