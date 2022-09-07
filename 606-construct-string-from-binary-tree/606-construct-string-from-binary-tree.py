7fd4d6d5e037cd4f157d929e79b86a637214af13
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-07 15:21:20 -0400

    Time: 50 ms (95.87%), Space: 16.6 MB (10.51%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ""
+        
         node_parts = [f"{root.val}"]
         
-        if root.left is not None:
+        if root.left is not None or root.right is not None:
             node_parts.append(f"({self.tree2str(root.left)})")
             
         if root.right is not None:
-            if root.left is None:
-                node_parts.append("()")
             node_parts.append(f"({self.tree2str(root.right)})")
             
         return ''.join(node_parts)
