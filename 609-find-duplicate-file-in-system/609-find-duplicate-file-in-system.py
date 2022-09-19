5bfbd20ba49e6314f0d54bc659b3b4844f203088
Addison Hernandez <addison.hernandez@gmail.com>
2022-09-19 16:15:23 -0400

    Time: 153 ms (50.42%), Space: 24 MB (54.47%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -3,9 +3,10 @@ class Solution:
         contents_to_files = defaultdict(list)
         
         for path in paths:
-            directory, *files = path.split(" ")
+            directory, *files = path.split()
             for file in files:
-                fname, contents = re.match(r"(.*\.txt)\((.*)\)", file).groups()
+                # fname, contents = re.match(r"(.*\.txt)\((.*)\)", file).groups()
+                fname, contents = file.split("(")
                 contents_to_files[contents].append(f"{directory}/{fname}")
         
         return [fnames for fnames in contents_to_files.values() if len(fnames) > 1]
