9fb488a9ed86dd9402c6bf406a2dd19302e3ac57
Addison Hernandez <addison.hernandez@gmail.com>
2022-10-03 16:57:57 -0400

    Time: 19 ms (42.81%), Space: 3 MB (58.64%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.go
@@ -24,20 +24,3 @@ func max(a, b int) int {
     }
     return b
 }
-
-/*
-  for (const c of s) {
-    counts[c] = (counts[c] ?? 0) + 1;
-    tail += 1;
-    
-    while (counts[c] > 1) {
-      counts[s[head]] -= 1;
-      head += 1;
-    }
-    
-    maxLen = Math.max(maxLen, tail - head);
-  }
-  
-  return maxLen;
-};
-*/
