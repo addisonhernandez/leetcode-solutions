12be22ea3eb7d7b082e8314d4513c0045e217c65
Addison Hernandez <addison.hernandez@gmail.com>
2022-10-05 07:29:38 -0400

    Time: 81 ms (65.15%), Space: 16.7 MB (69.24%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.py
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.py
@@ -6,17 +6,29 @@
 #         self.right = right
 class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
-        if root is None:
-            return
+        # if root is None:
+        #     return
         
         if depth == 1:
             return TreeNode(val, root, None)
         
-        if depth == 2:
-            root.left = TreeNode(val, root.left, None)
-            root.right = TreeNode(val, None, root.right)
-            return root
+        # if depth == 2:
+        #     root.left = TreeNode(val, root.left, None)
+        #     root.right = TreeNode(val, None, root.right)
+        #     return root
         
-        self.addOneRow(root.left, val, depth - 1)
-        self.addOneRow(root.right, val, depth - 1)
+        # self.addOneRow(root.left, val, depth - 1)
+        # self.addOneRow(root.right, val, depth - 1)
+        # return root
+        
+        level = [root]
+        
+        while depth > 2:
+            level = [child for node in level for child in (node.left, node.right) if child is not None]
+            depth -= 1
+        
+        for node in level:
+            node.left = TreeNode(val, node.left, None)
+            node.right = TreeNode(val, None, node.right)
+        
         return root
