2b7dfceae96a0ecd55e0a6aa96e3e122f22249f7
Addison Hernandez <addison.hernandez@gmail.com>
2022-10-13 09:03:31 -0400

    Time: 39 ms (93.00%), Space: 6.6 MB (88.00%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.go
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.go
@@ -1,11 +1,13 @@
 import "sort"
 
 func largestPerimeter(nums []int) int {
-    sort.Slice(nums, func(i, j int) bool { return nums[j] < nums[i] })
+    // sort.Slice(nums, func(i, j int) bool { return nums[j] < nums[i] })
+    sort.Ints(nums)
     
-    for i := 0; i < len(nums) - 2; i++ {
-        if nums[i] < nums[i+1] + nums[i+2] {
-            return nums[i] + nums[i+1] + nums[i+2]
+    // for i := 0; i < len(nums) - 2; i++ {
+    for i := len(nums) - 1; i > 1; i-- {
+        if nums[i] < nums[i-1] + nums[i-2] {
+            return nums[i] + nums[i-1] + nums[i-2]
         }
     }
     
