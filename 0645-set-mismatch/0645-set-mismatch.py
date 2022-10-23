4cfeb92e24174481f06b59ba144f4206f94d28b0
Addison Hernandez <addison.hernandez@gmail.com>
2022-10-23 09:16:32 -0400

    Time: 467 ms (43.23%), Space: 15.5 MB (69.79%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,10 +1,24 @@
-from collections import Counter
-
-
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        unique_sum = sum({*nums})
-        intended_sum = len(nums) * (len(nums) + 1) // 2
-        actual_sum = sum(nums)
+        n = len(nums)
         
-        return [actual_sum - unique_sum, intended_sum - unique_sum]
+        # # Difference of sums:
+        # unique_sum = sum({*nums})
+        # intended_sum = n * (n + 1) // 2
+        # actual_sum = sum(nums)
+        # return [actual_sum - unique_sum, intended_sum - unique_sum]
+        
+        # In-place marking:
+        dupe = 0
+        miss = 0
+        
+        for i, num in enumerate(nums, 1):
+            nums[num % n - 1] += n
+        
+        for i, num in enumerate(nums, 1):
+            if num <= n:
+                miss = i
+            elif num > 2 * n:
+                dupe = i
+        
+        return [dupe, miss]
